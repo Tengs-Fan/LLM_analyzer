85922222136e00fb8b70f45d558fa49afece6f8c
Tengs Penkwe <tengspenkwe@gmail.com>
2023-10-27 00:18:32 -0700

    change all the name to make them consistent

--- MediaMined/db/mongo.py
+++ MediaMined/db/mongo.py
@@ -1,5 +1,4 @@
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 
 client = MongoClient('localhost', 27017)
 reddit = client['reddit']
@@ -8,20 +7,28 @@ youtube = client['database']
 #######################################
 # Text transcript of Youtube Video
 #######################################
-video_texts = youtube['video_texts']
+youtube_dictation = youtube['video_texts']
+youtube_comments = youtube['video_comments']
 
-def insert2video_text(document):
+def youtube_insert_dictation(document):
     try:
-        video_texts.insert_one(document)
+        youtube_dictation.insert_one(document)
     except Exception as e:
-        print(f"Could not store transcript in MongoDB. Error: {e}")
+        print(f"Could not store dication of video in MongoDB. Error: {e}")
 
-def find_in_video_text(video_id):
-     return video_texts.find_one({"_id": video_id})
+def youtube_insert_comments(document):
+    try:
+        youtube_comments.insert_one(document)
+    except Exception as e:
+        print(f"Could not store comments of video in MongoDB. Error: {e}")
 
-def get_from_id(video_id):
-    document = video_texts.find_one({'_id': video_id})
-    return document
+def youtube_get_dictation_by_video_id(video_id):
+    return youtube_dictation.find_one({"_id": video_id})
+
+def youtube_get_comment_by_video_id(video_id):
+    comments_cursor = youtube_comments.find({'video_id': video_id})
+    comments_list = list(comments_cursor)
+    return comments_list
 
 #######################################
 # Reddit Comments
@@ -29,22 +36,22 @@ def get_from_id(video_id):
 reddit_posts = reddit['reddit_post']
 reddit_comments = reddit['reddit_comments']
 
-def insert_post(document):
+def reddit_insert_post(document):
     try:
         reddit_posts.insert_one(document)
     except Exception as e:
         print(f"Could not store post in MongoDB. Error: {e}")
 
-def find_post(post_id):
+def reddit_get_post(post_id):
      return reddit_posts.find_one({"_id": post_id})
 
-def insert_comment(document):
+def reddit_insert_comment(document):
     try:
         reddit_comments.insert_one(document)
     except Exception as e:
         print(f"Could not store comment in MongoDB. Error: {e}")
 
-def find_comment_by_id(comment_id):
+def reddit_get_comment_by_id(comment_id):
     """
     Searches for and returns a comment document based on the comment ID.
 
@@ -54,7 +61,7 @@ def find_comment_by_id(comment_id):
     comment = reddit_comments.find_one({'_id': comment_id})
     return comment
 
-def find_comments_by_post(post_id):
+def reddit_get_comments_by_post_id(post_id):
     """
     Searches for and returns all comments belonging to a specific post_id.
 
@@ -65,7 +72,7 @@ def find_comments_by_post(post_id):
     comments_list = list(comments_cursor)
     return comments_list
 
-def get_all_posts():
+def reddit_get_all_posts():
     """
     Retrieves all documents from the reddit_post collection.
 
