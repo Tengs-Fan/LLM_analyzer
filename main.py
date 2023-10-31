9782d05998633a4902fec430496ce93caa06ce0f
Tengs Penkwe <tengspenkwe@gmail.com>
2023-10-31 00:13:43 -0700

    NOT WORKING yet: add youtube section

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def index():
 @main.route('/reddit')
 @login_required
 def reddits():
-    logger.info("access reddit database")
     total_posts = mongo.get_total_posts_count()
     posts = mongo.get_all_posts()
     average_upvotes = 0.7
@@ -23,6 +22,7 @@ def reddits():
 @main.route('/reddit/<post_id>')
 @login_required
 def reddit_post(post_id):
+    logger.info("access reddit posts")
     # Fetch the post details from the database
     post = mongo.find_post(post_id)
     # Fetch the comments associated with this post
@@ -36,5 +36,20 @@ def submit_reddit_url():
 
     reddit.get_content(url);
     
-    return redirect(url_for('reddits'))  # Redirect back to the index page
+    return redirect(url_for('main.reddits'))  # Redirect back to the index page
 
+# @main.route('/youtube')
+# @login_required
+# def youtubes():
+#     # total_videos = mongo.get_total_videos_count()
+#     # videos = mongo.get_all_videos()
+#     # average_likes = 0.9  # Example, adjust as needed
+#     return render_template('youtube.html', videos=videos, total_videos=total_videos, average_likes=average_likes)
+
+# @main.route('/youtube/<video_id>')
+# @login_required
+# def youtube_video(video_id):
+#     logger.info("access YouTube video")
+#     video = mongo.find_video(video_id)
+#     comments = mongo.find_comments_by_video(video_id)
+#     return render_template('youtube_video.html', video=video, comments=comments)
