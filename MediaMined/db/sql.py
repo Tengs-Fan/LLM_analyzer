e853b2dd764f3aec5ac7370408b6b30f37e6e9b8
Tengs Penkwe <tengspenkwe@gmail.com>
2023-10-31 00:05:55 -0700

    currently not used, add commented textt of SQLAlchemy implementation

--- MediaMined/db/sql.py
+++ MediaMined/db/sql.py
@@ -40,3 +40,54 @@ def insert_comment_data(comment):
     ''', data_tuple)
 
     conn.commit()
+
+# from sqlalchemy import create_engine, Column, String, Integer, MetaData, Table
+# from sqlalchemy.orm import sessionmaker
+
+# DATABASE_URL = "sqlite:///youtube_comments.db"  # SQLite database URL
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(bind=engine)
+
+# metadata = MetaData()
+
+# from sqlalchemy.ext.declarative import declarative_base
+
+# Base = declarative_base()
+
+# class Comment(Base):
+#     __tablename__ = "comments"
+#     
+#     video_id = Column(String, nullable=False)
+#     comment_id = Column(String, primary_key=True)
+#     comment_text = Column(String, nullable=False)
+#     author_display_name = Column(String, nullable=True)
+#     like_count = Column(Integer, nullable=True)
+#     published_at = Column(String, nullable=False)
+#     total_reply_count = Column(Integer, nullable=True)
+
+# Base.metadata.create_all(engine)
+
+# def insert_comment_data(comment):
+#     """
+#     Insert a comment's data into the database.
+
+#     :param comment_data: A dictionary containing the comment data.
+#     """
+#     # Create a new comment instance
+#     new_comment = Comment(
+#         video_id=comment['video_id'],
+#         comment_id=comment['comment_id'],
+#         comment_text=comment['comment_text'],
+#         author_display_name=comment['author_display_name'],
+#         like_count=comment['like_count'],
+#         published_at=comment['published_at'],
+#         total_reply_count=comment['total_reply_count']
+#     )
+
+#     # Insert the data into the database
+#     session = SessionLocal()
+#     session.add(new_comment)
+#     session.commit()
+#     session.close()
+
+
