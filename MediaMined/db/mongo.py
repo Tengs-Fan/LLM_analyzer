a30b088d5ba8d8f6b465776dae7c1e3863caf699
Tengs Penkwe <tengspenkwe@gmail.com>
2023-11-10 19:49:59 -0800

    new functions

--- MediaMined/db/mongo.py
+++ MediaMined/db/mongo.py
@@ -4,7 +4,7 @@ client = MongoClient('localhost', 27017)
 reddit = client['reddit']
 youtube = client['youtube']
 
-def get_items_by_id(collection, key, id):
+def _get_items_by_id(collection, key, id):
     """
     Searches for and returns all comments belonging to a specific post_id.
 
@@ -22,11 +22,40 @@ def get_items_by_id(collection, key, id):
     else:
         raise Exception(f"can't find this {id} of {key} in {collection}")
 
-def test_exist_in_collection(collection, key, id):
+def _test_exist_in_collection(collection, key, id):
     """
     :return: If the id exist in collection, True; not exist, False
     """
     return collection.count_documents({ key: id }, limit = 1) != 0
+
+def _update_document(collection, query_key, query_value, update_data):
+    """
+    Update documents in a MongoDB collection based on a query.
+
+    :param collection: The MongoDB collection to update.
+    :param query_key: The field key to query the documents.
+    :param query_value: The value to match for the query_key in the documents.
+    :param update_data: A dictionary containing the fields to update and their new values.
+    """
+    # Step 1: Retrieve all matching documents from MongoDB
+    cursor = collection.find({query_key: query_value})
+
+    if cursor is None:
+        raise Exception(f"Can't find documents with {query_key} = {query_value} in {collection.name}")
+
+    # Step 2: Update each matching document
+    collection.update_many(
+        {query_key: query_value},
+        {'$set': update_data}
+    )
+
+def _get_all(collection):
+    """
+    Retrieves all documents from the given collection.
+
+    :return: A corsor of documents.
+    """
+    return collection.find()
 
 #######################################
 # Text transcript of Youtube Video
@@ -46,6 +75,9 @@ def youtube_insert_comments(document):
     except Exception as e:
         print(f"Could not store comments of video in MongoDB. Error: {e}")
 
+def youtube_update_dictation(id, data):
+    return _update_document(youtube_dictation, '_id', id, data)
+
 def youtube_exist_dictation(video_id):
     return youtube_dictation.count_documents({ '_id': video_id }, limit = 1) != 0
 
@@ -56,7 +88,10 @@ def youtube_exist_comment(comment_id):
     return youtube_comments.count_documents({ '_id': comment_id }, limit = 1) != 0
 
 def youtube_get_comment_by_video_id(video_id):
-    return get_items_by_id(youtube_comments, 'video_id', video_id);
+    return _get_items_by_id(youtube_comments, 'video_id', video_id);
+
+def youtube_get_all_dictations():
+    return _get_all(youtube_dictation)
 
 #######################################
 # Reddit Comments
@@ -71,13 +106,13 @@ def reddit_insert_post(document):
         print(f"Could not store post in MongoDB. Error: {e}")
 
 def reddit_exist_post(post_id):
-    return test_exist_in_collection(reddit_posts, '_id', post_id)
+    return _test_exist_in_collection(reddit_posts, '_id', post_id)
 
 def reddit_get_post(post_id):
      return reddit_posts.find_one({"_id": post_id})
 
 def reddit_exist_comment(comment_id):
-    return test_exist_in_collection(reddit_comments, '_id', comment_id)
+    return _test_exist_in_collection(reddit_comments, '_id', comment_id)
 
 def reddit_insert_comment(document):
     try:
@@ -90,7 +125,7 @@ def reddit_get_comment_by_id(comment_id):
     return comment
 
 def reddit_get_comments_by_post_id(post_id):
-    return get_items_by_id(reddit_comments, 'post_id', post_id)
+    return _get_items_by_id(reddit_comments, 'post_id', post_id)
 
 def reddit_get_top_comments_by_parent_id(parent_id, top_n = 50):
     comments_cursor = reddit_comments.find({'parent_id': parent_id}).sort('upvotes', -1).limit(top_n)
@@ -104,14 +139,7 @@ def reddit_get_top_comments_by_post_id(post_id, top_n = 50):
     return comments_list
 
 def reddit_get_all_posts():
-    """
-    Retrieves all documents from the reddit_post collection.
-
-    :return: A list of documents.
-    """
-    cursor = reddit_posts.find()
-    return list(cursor)
+    return _get_all(reddit_posts)
 
 def reddit_get_total_posts_count():
     return reddit_posts.count_documents({})
-
