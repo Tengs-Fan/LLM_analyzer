2f6dc027c8dff5f3c4939f165d7e5ed41a7e7227
Tengs Penkwe <tengspenkwe@gmail.com>
2023-11-10 19:50:17 -0800

    update video

--- MediaMined/script.py
+++ MediaMined/script.py
@@ -1,4 +1,17 @@
-from .analysis import * 
+from .llm import * 
+from .youtube import *
+from .db import mongo
 # script to directly run in CLI
 
-summarize_video_dictation("test", 10)
+# summarize_video_dictation("test", 10)
+get_stat("B2NkjV-hSwk")
+
+def update_videos():
+    for video in mongo.youtube_get_all_dictations():
+        video_id = video['_id']
+        stat = get_stat(video_id)
+        mongo.youtube_update_dictation(video_id, stat)
+
+update_videos()
+
+
