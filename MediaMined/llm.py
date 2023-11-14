48bf23cf752f1f8ab6cd2469c99be6c1832d98be
Tengs Penkwe <tengspenkwe@gmail.com>
2023-11-14 15:01:36 -0800

    LLM new changes

--- MediaMined/llm.py
+++ MediaMined/llm.py
@@ -19,7 +19,7 @@ chatGPT4_100k = ChatOpenAI(
     )
 
 
-expert = "Assume you are an expert of Human Right and Hong Kong, You will analyze online discussions related to state surveillance in Hong Kong."
+expert = "Assume you are an expert of Human Right and Hong Kong, You will analyze online discussions related to state surveillance in Hong Kong. You should answer in JSON format"
 
 system_template = ("{system}")
 system_prompt = SystemMessagePromptTemplate.from_template(system_template)
@@ -31,6 +31,97 @@ task_template = (
 """)
 task_prompt = HumanMessagePromptTemplate.from_template(task_template)
 
+related_post = """
+2019 Jan 01 17:38
+Upvotes: 1034
+Title: Impact of National Security Law on Hong Kong's Freedom
+
+I'm increasingly concerned about the state surveillance in Hong Kong under the new National Security Law. It feels like our freedoms are being stripped away. What are your thoughts?
+
+2019 Jan 01 20:12
+By Marcus123
+Upvotes: 534 
+
+It's alarming to see how quickly our rights are being eroded.
+---
+2019 Jan 02 10:56
+By 香港榮光
+Upvotes: 345 
+
+This is just the beginning. More oppressive measures might be on the way.
+---
+2019 Jan 01 17:38
+By Nichchk
+Upvotes: 275 
+
+I think the law is necessary for national security, but I'm worried about privacy.
+"""
+
+reply_related = (
+""" {
+  "Topic": ["National Security Law", "Privacy", "Human Rights", "Protest"],
+  "Attitudes": [
+    {
+      "Topic": "Surveillance",
+      "Relevance": 10,
+      "Sentiment": -7,
+    },
+    {
+      "Topic": "Security Law",
+      "Relevance": 10,
+      "Sentiment": -8,
+    },
+    {
+      "Topic": "Police",
+      "Relevance": 9,
+      "Sentiment": -9,
+    },
+    {
+      "Topic": "Resistance",
+      "Relevance": 8,
+      "Sentiment": 5,
+    },
+    {
+      "Topic": "Immigration",
+      "Relevance": 6,
+      "Sentiment": -3,
+    }
+  ],
+  "OtherWords": "The general tone in discussions indicates a growing unease about personal freedoms and the future of democracy in Hong Kong."
+}""")
+# example_prompt = AIMessagePromptTemplate.from_template(example_template)
+
+
+unrelated_post = """2019 Oct 09 07:38
+Upvotes: 1034
+Title: Best Hiking Trails in Hong Kong
+    
+Comments
+---
+2019 Oct 09 10:12
+By frauenarzZzt
+Upvotes: 123 
+
+Dragon's Back is a must-try, offers amazing views!
+---
+2019 Oct 09 07:40
+By Nichchk
+Upvotes: 75 
+
+Lion Rock has a challenging path but it's totally worth it.
+---
+2019 Oct 09 11:08
+By Dr_Rockso89
+Upvotes: 1534 
+
+Don't forget to check out the MacLehose Trail, it's fantastic."""
+reply_unrelated = """{
+  "Topic": [Hiking", "Outdoor Activities"],
+  "Ralted": false
+  "OtherWords": "This one is totally unrelated"
+}"""
+
+# analyze_reddit_post(unrelated_post)
 input_prompts = ChatPromptTemplate.from_messages(
     [system_prompt, task_prompt]
 )
@@ -44,8 +135,8 @@ def summarize_video_dictation(dictation, aimed_words):
 
 # Get a analysis of reddit post 
 def analyze_reddit_post(post):
-    task = "example task"
-    end = "please give your analysis"
+    task = "Analyze the sentiment and themes in the provided Reddit post and comments related to state surveillance in Hong Kong."
+    end = "Provide your analysis in JSON format."
 
     return send_request(False, input_prompts, expert, task, post, end)
 
